868e9b9a430d7c2c76aeac38a04a1f34daad0e70
Quezia <queziaamenezes@gmail.com>
2022-05-05 16:21:15 -0300

    fix

--- src/main/java/br/com/sqs_producer/App.java
+++ src/main/java/br/com/sqs_producer/App.java
@@ -1,11 +1,11 @@
 package br.com.sqs_producer;
 
-
+import java.time.LocalDate;
 
 import br.com.sqs_producer.services.SQSSendMessage;
 
 public class App {
   public static void main(String[] args) {
-    SQSSendMessage.sendMessage("Producer");
+    SQSSendMessage.sendMessage("Producer desacoplado (teste) - " + LocalDate.now());
   }
 }
